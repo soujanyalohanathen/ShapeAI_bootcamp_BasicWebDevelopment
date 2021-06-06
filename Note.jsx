f7fc1847c55d59806b0a6c7fd8617924ab91d5a9
Soujanya Lohanathen <58034992+Soujanya9823@users.noreply.github.com>
2021-06-06 20:10:10 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        As a note I give here my feedback on the bootcamp. That was an amazing 7 days of bootcamp where I gained many things I never know before. The speed of teaching was exaclty correct for beginners. Thank you and do a lot more like this.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
